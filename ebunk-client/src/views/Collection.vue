5e7c81e1e9132d9da06c7cbbb75f2513a1675b8e
Harish Dhulipalla <harish.d14@iiits.in>
2020-12-27 21:58:25 +0530

    Client code vue js check-in (#3)
    * Server check in
    
    * CRUD Operations api addition
    
    * README update
    
    * ebunk client check in

---
+++ ebunk-client/src/views/Collection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="collection">
+      <h1 class="subheading grey--text">Collection</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Collection'
+}
+</script>
